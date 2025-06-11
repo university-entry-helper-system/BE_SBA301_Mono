cb947f8a85596896f721f490397745710cb7b304
nhducminhqt <nhducminhqt@gmail.com>
2025-06-11 13:27:27 +0700

    feat(BE): initialize the product

---
+++ SBA_M/src/main/java/com/example/SBA_M/SbaMApplication.java
@@ -0,0 +1,13 @@
+package com.example.SBA_M;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbaMApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbaMApplication.class, args);
+	}
+
+}
