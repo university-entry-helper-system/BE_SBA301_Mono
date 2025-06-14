16a22ee1e648333cba1184349e32995ac84e3d0e
huypqse <huypqse@gmail.com>
2025-06-14 14:35:03 +0700

    feat: add annotation

--- SBA_M/src/main/java/com/example/SBA_M/repository/RoleRepository.java
+++ SBA_M/src/main/java/com/example/SBA_M/repository/RoleRepository.java
@@ -3,6 +3,11 @@ package com.example.SBA_M.repository;
 
 import com.example.SBA_M.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
 }
--- SBA_M/src/main/java/com/example/SBA_M/repository/UniversityCategoryRepository.java
+++ SBA_M/src/main/java/com/example/SBA_M/repository/UniversityCategoryRepository.java
@@ -2,6 +2,8 @@ package com.example.SBA_M.repository;
 
 import com.example.SBA_M.entity.UniversityCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UniversityCategoryRepository extends JpaRepository<UniversityCategory, Long> {
 }
