3a2454fb6916f9f1df8c1d4656e5a63067336f7a
Nguyễn Sơn Nam <128161951+sonnamnguyen@users.noreply.github.com>
2025-06-29 00:20:46 +0700

    Feat: Update Province.java

--- SBA_M/src/main/java/com/example/SBA_M/entity/commands/Province.java
+++ SBA_M/src/main/java/com/example/SBA_M/entity/commands/Province.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 
 @Data
 @Entity
-@Table(name = "universities")
+@Table(name = "province")
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(callSuper = true)
