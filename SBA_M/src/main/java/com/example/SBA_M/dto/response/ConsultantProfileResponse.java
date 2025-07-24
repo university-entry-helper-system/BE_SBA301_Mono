04ca87d7104796a929a25f08a3dbe9e986e48c2d
huypqse <huypqse@gmail.com>
2025-07-24 08:29:26 +0700

    feat: add mapper for consulation status

--- SBA_M/src/main/java/com/example/SBA_M/dto/response/ConsultantProfileResponse.java
+++ SBA_M/src/main/java/com/example/SBA_M/dto/response/ConsultantProfileResponse.java
@@ -1,7 +1,8 @@
 package com.example.SBA_M.dto.response;
 
 
-import com.example.SBA_M.utils.Gender;
+import com.example.SBA_M.utils.ConsultationStatus;
+import com.example.SBA_M.utils.StatusConsultant;
 import lombok.Data;
 
 import java.util.List;
@@ -12,5 +13,6 @@ public class ConsultantProfileResponse {
     private UUID accountId;
     private String fullName;
     private String bio;
+    private StatusConsultant status;
     private List<MajorResponse> specialties;
 }
